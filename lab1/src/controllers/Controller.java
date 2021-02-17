b02e293b0373e4e0e295a66f9ae28dd675adcd97
RuslanPark <0905.ruslan.pak.2001@mail.ru >
2021-02-18 01:17:32 +0600

    Bug's fixed Descriptions added Beauty effects added

--- lab1/src/controllers/Controller.java
+++ lab1/src/controllers/Controller.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.LineChart;
@@ -10,29 +9,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.util.Pair;
 import model.*;
-
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class Controller {
 
-    @FXML
     public ComboBox<String> comboMethodsBox;
-    @FXML
     public ComboBox<String> comboLineBox;
-    @FXML
     public Button buildButton;
-    @FXML
     public LineChart<Number, Number> lineChart;
+
+    public List< Pair<Number, Number> > list;
+    CalculationMethod calculationMethod = null;
+
 
     public void initialize() {
         ObservableList<String> methods = FXCollections.observableArrayList("Dichotomy", "Golden ratio", "Fibonacci", "Parabolas","Brent's");
         comboMethodsBox.setItems(methods);
 
     }
-
-    public List< Pair<Number, Number> > list;
-    CalculationMethod calculationMethod = null;
 
     public void methodChosen() {
         switch (comboMethodsBox.getValue()) {
@@ -45,8 +39,15 @@ public class Controller {
     }
 
     public void buildGraph() {
+        // Create alert dialog for change notification
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+
         try {
+            // Calculate plot points by chosen method
             list = calculationMethod.calculate();
+
+            // Collect points and iterations count
             ObservableList<String> lines = FXCollections.observableArrayList();
             for (int i = 0; i < list.size(); ++i) {
                 String str = String.valueOf(i + 1);
@@ -54,36 +55,37 @@ public class Controller {
             }
             comboLineBox.setItems(lines);
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
             alert.setTitle("Success!");
-            alert.setHeaderText(null);
             alert.setContentText("Graphic has benn build, chose step in the next comboBox!");
             alert.showAndWait();
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Error!");
-            alert.setHeaderText(null);
             alert.setContentText("Chose calculation method before you build the graphic!");
             alert.showAndWait();
         }
     }
 
     public void lineChosen() {
-        int pos = Integer.parseInt(comboLineBox.getValue());
-        Pair<Number, Number> pairChosen = list.get(pos - 1);
-        float a = (float) pairChosen.getKey();
-        float b = (float) pairChosen.getValue();
+        int chosenLinePosition = Integer.parseInt(comboLineBox.getValue());
+        Pair<Number, Number> chosenPoint = list.get(chosenLinePosition - 1);
+        float a = (float) chosenPoint.getKey();
+        float b = (float) chosenPoint.getValue();
+
+        // Clear chart
         lineChart.getData().clear();
 
+        // Add points (XYChart.Data) in special container (XYChart.Series)
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
         while (a < b) {
             float result = (float) (a * a + Math.exp(-0.35 * a));
             a += 0.01;
             series.getData().add(new XYChart.Data<>(a, result));
         }
-        series.setName("X= " + pairChosen.getKey() + " f(X)= " + pairChosen.getValue());
+        // Set legend format and load container with points
+        series.setName("Left X= " + chosenPoint.getKey() + "  |  Right X= " + chosenPoint.getValue());
         lineChart.getData().add(series);
     }
 
--- lab1/src/model/Dichotomy.java
+++ lab1/src/model/Dichotomy.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Dichotomy extends CalculationMethod {
 
     float left, right;
-    float delta = (float) 0.000001;
+    float delta = (float) 0.0001;
     float epsilon = (float) 0.0001;
 
     public Dichotomy() {
@@ -16,8 +16,11 @@ public class Dichotomy extends CalculationMethod {
     }
 
     public boolean isMinimum(float x1, float x2) {
+        // Calculate f(X1) and f(X2)
         float result1 = (float) (x1 * x1 + Math.exp(-0.35 * x1));
         float result2 = (float) (x2 * x2 + Math.exp(-0.35 * x2));
+
+        // Is f(X1) <= f(X2)
         return result1 <= result2;
     }
 
@@ -25,16 +28,20 @@ public class Dichotomy extends CalculationMethod {
     public List< Pair<Number, Number> > calculate() {
 
         List< Pair<Number, Number> > list = new ArrayList<>();
-
         list.add(new Pair<>(left, right));
+
         while ((right - left) / 2 > epsilon) {
+            // Calculate X1 and X2
             float x1 = (left + right - delta) / 2;
             float x2 = (left + right + delta) / 2;
+
+            // Check the condition
             if (isMinimum(x1, x2)) {
                 right = x2;
             } else {
                 left = x1;
             }
+
             list.add(new Pair<>(left, right));
         }
 
