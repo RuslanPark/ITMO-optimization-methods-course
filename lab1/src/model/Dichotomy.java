6a0e2780646ac57056154fdb01557bc3f966db6a
Roman <roman-z@list.ru>
2021-03-02 19:52:34 +0300

    A small fix Dichotomy

--- lab1/src/model/Dichotomy.java
+++ lab1/src/model/Dichotomy.java
@@ -3,7 +3,7 @@ package model;
 import javafx.util.Pair;
 
 public class Dichotomy extends CalculationMethod {
-    final private double delta = 0.0001;
+    final private double delta = 0.000001;
     final private double epsilon = 0.0001;
 
     @Override
@@ -11,12 +11,10 @@ public class Dichotomy extends CalculationMethod {
 
         graphPoints.add(new Pair<>(left, right));
 
-        double x1 = (left + right - delta) / 2;
-        double x2 = (left + right + delta) / 2;
         while ((right - left) / 2 > epsilon) {
             // Calculate X1 and X2
-            x1 = (left + right - delta) / 2;
-            x2 = (left + right + delta) / 2;
+            double x1 = (left + right - delta) / 2;
+            double x2 = (left + right + delta) / 2;
 
             // Check the condition
             if (isFunctionLess(x1, x2)) {
@@ -28,6 +26,6 @@ public class Dichotomy extends CalculationMethod {
             graphPoints.add(new Pair<>(left, right));
         }
 
-        return (x1 + x2) / 2;
+        return (left + right) / 2;
     }
 }
