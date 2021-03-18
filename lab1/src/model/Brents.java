bf8a26cb360d6cc0da15b7a7164359d243a36aad
Dmitriy Ivanov <akolyx@gmail.com>
2021-03-18 22:43:02 +0300

    Brent's method code updated

--- lab1/src/model/Brents.java
+++ lab1/src/model/Brents.java
@@ -4,10 +4,9 @@ import javafx.util.Pair;
 import java.util.List;
 
 public class Brents extends CalculationMethod {
-    private final double tol = 0.0000001;
-
     @Override
     public double calculate() {
+        double tol = epsilon;
 
         // Initial parameters; a < b
         double a = left, b = right;
@@ -38,6 +37,8 @@ public class Brents extends CalculationMethod {
         xm = (a + b) / 2;
         tol1 = eps * Math.abs(x) + tol3;
         t2 = 2.0 * tol1;
+
+        HashMap<String, Double> hashMap = new HashMap<>();
 
         while (Math.abs(x - xm) > (t2 - (b - a) / 2)) {
             p = q = r = 0.0;
@@ -73,60 +74,51 @@ public class Brents extends CalculationMethod {
                 }
             } else {
                 // Golden ratio step
-                if (x < xm) {
-                    e = b - x;
-                } else {
-                    e = a - x;
-                }
+                e = (x < xm ? b : a) - x;
+
                 d = c * e;
             }
 
             // f must not be evaluated too close to x
-            if (Math.abs(d) >= tol1) {
-                u = x + d;
-            } else {
-                if (d > 0.0) {
-                    u = x + tol1;
-                } else {
-                    u = x - tol1;
-                }
-            }
+            u = x + (Math.abs(d) >= tol1 ? d : (d > 0.0 ? tol1 : -tol1));
 
             fu = Function.calculateFunctionValue(u);
 
-            // Update a, b, v, w, and x
-            if (fx <= fu) {
-                if (u < x) {
-                    a = u;
-                } else {
-                    b = u;
-                }
-            }
+            // Add current step points for building graph
+            hashMap.put("left", a);
+            hashMap.put("right", b);
+            hashMap.put("x", x);
+            hashMap.put("u", u);
+            graphPoints.add(hashMap);
+            hashMap.clear();
 
+            // Update a, b, v, w, and x
             if (fu <= fx) {
-                if (u < x) {
-                    b = x;
-                } else {
-                    a = x;
-                }
+                (u < x ? b : a) = x;
+
                 v = w;
                 fv = fw;
+
                 w = x;
                 fw = fx;
+
                 x = u;
                 fx = fu;
             } else {
+                (u < x ? a : b) = u;
+
                 if ((fu <= fw) || (w == x)) {
                     v = w;
                     fv = fw;
+
                     w = u;
                     fw = fu;
-                } else if ((fu > fv) && (v != x) && (v != w)) {
-                } else {
+                } else if ((fu > fv) && (v == x) && (v == w)) {
                     v = u;
                     fv = fu;
                 }
             }
+
             xm = (a + b) / 2;
             tol1 = eps * Math.abs(x) + tol3;
             t2 = 2.0 * tol1;
